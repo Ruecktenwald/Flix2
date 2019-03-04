bb46f760959929f610c9e7c7531b38ddbbd78534
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-03-04 13:26:28 -0500

    add user routes, create, destroy, edit

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526220418) do
+ActiveRecord::Schema.define(version: 20190302173815) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +34,14 @@ ActiveRecord::Schema.define(version: 20160526220418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
